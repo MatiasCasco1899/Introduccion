e5a2bdda06fa416590686f33248613f38f8ea085
Matias <maticasco120@gmail.com>
2024-03-20 09:11:52 -0300

    holaaa

---
+++ imprimir2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Segundo Dia</title>
+    <link rel="stylesheet" href="hola.css">
+</head>
+<body>
+    <form action= "index.php" method="post">
+        <h1>Hola</h1>
+    <?php echo <<<END
+    En un mundo conectado, donde cada sugundo se envia millones de correos electronicos...
+    END;    ?>
+</body>
+</html>
