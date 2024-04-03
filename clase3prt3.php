7eb80b13ed5821825f02d39a4a21f118b29bb14a
Matias Casco <maticasco120@gmail.com>
2024-04-03 09:00:15 -0300

    Clase 3 prt3

---
+++ clase3prt3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Mi tercer archivo y tercer parte >:C</h1>
+<h2>Tipos de dato variaw</h2>
+    <?php
+        $nombre = 'Matias';
+        $edad = 18; 
+        $altura = 1.99;
+        $mayorEdad = true; 
+    ?>
+</body>
+</html>
