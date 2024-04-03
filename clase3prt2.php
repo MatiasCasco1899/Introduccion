5aebac1c3ec9e3ef797429497b3a3b9f5f5d4550
Matias Casco <maticasco120@gmail.com>
2024-04-03 08:34:55 -0300

    parte2

---
+++ clase3prt2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Mi segundo archivo </h1>
+    <?php
+    echo <<<END
+    En un mundo conectado, donde cada segundo se envia millones de correos electronicos...
+    END;
+    ?> 
+    
+</body>
+</html>
