0e369f15da5e7361b492e0b39b6937b0aa243b04
Matias Casco <maticasco120@gmail.com>
2024-04-03 09:18:12 -0300

    Clase 3 prt4

---
+++ clase3prt4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Acividad Variables</h1>
+<?php
+        $nombreCompleto = "Juam Perez" // Falta el ;
+        $Edad = 25; // Esta bien
+        $estaTura = 1.75;
+        $numero = "10";
+        Apellido = ´Fernandez´ ; // Falta el simbolo "$" y no son las comillas correctas ('/")
+        pais_de_origen = "Uruguay" // Falta el simbolo "$" , falta ";" y nos puede confundir los guiones 
+        $salario = $2000 // No tiene que tener el "$"
+    ?>
+    
+</body>
+</html>
