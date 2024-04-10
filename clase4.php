615e20c4a71371fd8c0bd05cccf130b5746b875e
MatiasCasco1899 <maticasco120@gmail.com>
2024-04-10 08:57:50 -0300

    actividad10/04/24

--- clase4.php
+++ clase4.php
@@ -7,9 +7,11 @@
 </head>
 <body>
 <?php
-    <h1>Actividad</h1>
     define ('PI', 3.1416);
     const PI = 3.14;
+    // 1) Warning: Constant PI already defined in C:\xampp\htdocs\Introduccion\clase4.php on line 11
+    // 2) Como es una constante, no se puede cambiar el valor
+    // 3) Se usa la constante cuando tenes algun valor que sea permanente, porque no se puede cambiar
 ?>
 </body>
 </html>
