42c1c8ea69240e48e09f7b835d483589a0cf78f2
MatiasCasco1899 <maticasco120@gmail.com>
2024-04-10 09:13:45 -0300

    actividad10/04/24 parte2

---
+++ clase4prt2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $texto = "Holaa";
+    $numero = 23 ;
+    $float = 1.24 ;
+    print_r ( $texto );
+    var_dump ( $texto ); 
+?>
+</body>
+</html>
