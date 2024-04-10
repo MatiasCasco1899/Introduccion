48973fe4a2d955f93cc2c9b36d8e321222e27eae
MatiasCasco1899 <maticasco120@gmail.com>
2024-04-10 08:44:27 -0300

    ejercicio 4

---
+++ clase4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    <h1>Actividad</h1>
+    define ('PI', 3.1416);
+    const PI = 3.14;
+?>
+</body>
+</html>
